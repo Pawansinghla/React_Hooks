9b39f45b7138a0e574801f5038377e6c407c5e24
pawan singhla <pawansinghla300@gmail.com>
2020-05-02 23:32:03 +0530

    hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import ResourceList from './component/ResourceList';
 
-class App extends React.Component {
-  state = {
-    resourceName: 'posts'
-  }
+// class App extends React.Component {
+//   state = {
+//     resourceName: 'posts'
+//   }
 
-  render() {
-    return (
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
+//         <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
+//         <ResourceList resourceName={this.state.resourceName}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+const App=()=>{
+  const[resourceName,setResourceName]=useState('posts');
+  
+
+  //resourceName==currentState resourceName
+  //setResourceName==function that changes resourceName
+  //useState == function (parameter===initialState)
+
+  return(
       <React.Fragment>
-        <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-        <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-        <ResourceList resourceName={this.state.resourceName}/>
-      </React.Fragment>
-    )
-  }
+          <button onClick={() => setResourceName('posts')}>Posts</button>
+          <button onClick={() => setResourceName('todos')}>Todos</button>
+          <ResourceList resourceName={resourceName}/>
+        </React.Fragment>
+  );
 }
 export default App;
--- src/component/ResourceList.js
+++ src/component/ResourceList.js
@@ -1,34 +1,71 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = {
-    resources: [],
-  }
+    //CUSTOM HOOK
+    const useResource=(resourceName)={
+        
 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
-
-    this.setState({ resources: response.data });
-  }
-
-  async componentDidUpdate(prevProps) {
-    if(prevProps.resourceName !== this.props.resourceName) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
-
-      this.setState({ resources: response.data });
     }
-  }
 
-  render() {
-    return (
-      <ul>
-        {this.state.resources.map(resource => (
-          <li key={resource.id}>{resource.title}</li>
-        ))}
-      </ul>
+// class ResourceList extends React.Component {
+//   state = {
+//     resources: [],
+//   }
+
+//   async componentDidMount() {
+//     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+
+//     this.setState({ resources: response.data });
+//   }
+
+//   async componentDidUpdate(prevProps) {
+//     if(prevProps.resourceName !== this.props.resourceName) {
+//       const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+
+//       this.setState({ resources: response.data });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         {this.state.resources.map(resource => (
+//           <li key={resource.id}>{resource.title}</li>
+//         ))}
+//       </ul>
+//     )
+//   }
+// }
+const ResourceList=({resourceName})=>{
+    const[resources,setResource]=useState([]);
+
+    const fetchResource=async(resourceName)=>{
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resourceName}`);
+
+          // this.setState({ resources: response.data });
+          setResource(response.data);
+        
+    }
+    useEffect(()=>{
+        fetchResource(resourceName);
+
+
+    },[resourceName]);
+
+
+    return(
+        <ul>
+        {resources.map(resource=>(
+            <li key={resource.id}>{resource.title}</li>
+        ))
+
+        }
+
+        </ul>
+
     )
-  }
 }
+
+
 
 export default ResourceList;
